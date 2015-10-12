13872635cdb6f642533f07b36dfeadc34d9fcb9a
Ahmed Khaled MOhamed <ahmed_khaled6551@yahoo.com>
2015-10-12 13:37:28 +0200

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -1,7 +1,7 @@
 function love.conf(t)
     t.identity = nil                   -- The name of the save directory (string)
     t.version = "0.9.2"                -- The LÖVE version this game was made for (string)
-    t.console = true                 -- Attach a console (boolean, Windows only)
+    t.console = false                 -- Attach a console (boolean, Windows only)
  
     t.window.title = "Catching_Birds"        -- The window title (string)
     t.window.icon = nil                -- Filepath to an image to use as the window's icon (string)
